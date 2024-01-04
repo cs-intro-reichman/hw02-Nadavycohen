7a2ba32188962e0bfab421231abb8e45c0bb4ed2
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2024-01-04 06:38:38 -0500

    InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -5,5 +5,17 @@
 public class InOrder {
 	public static void main (String[] args) {
 		//// Write your code here
+		int a = (int)((Math.random()*(10+1)));
+		System.out.print(a + " ");
+
+		do{
+			int b = (int)(Math.random()*(10+1));
+			if(b>=a)
+			System.out.print(b+ " ");
+			else break;
+			a = b;
+		}
+		while (true);
+			
 	}
 }
