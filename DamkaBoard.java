0bee1c130b9a9188d4defb8f61ccc8d67447c6bf
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2024-01-04 06:37:35 -0500

    DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,5 +4,21 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
-	}
-}
+		int n = Integer.parseInt(args[0]);
+		int line = 1;
+		while (line <= n) {
+		 int x = 1;
+		 while ( x<= n){
+			 if (line%2==0) {
+				System.out.print(" *");
+			 } 
+			 else  {
+				 System.out.print("* ");
+			 }
+			 x=x+1;
+		 } 
+		 System.out.println();
+		 line = line + 1;
+		}
+		 }
+	 }
