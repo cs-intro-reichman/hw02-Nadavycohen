2444e9e9eaa45f2ea27193fdbf29dc831cd95e1a
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2024-01-04 06:39:01 -0500

    OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -6,5 +6,21 @@
 public class OneOfEach {
 	public static void main (String[] args) {
 		//// Put your code here
+		boolean b = false;
+		boolean g = false;
+		int count = 0;
+		while (!b || !g){
+			double prob = Math.random();
+			if (prob < 0.5){
+				g = true;
+				count++;
+				System.out.print("g ");
+			} else {
+				b = true;
+				count++;
+				System.out.print("b ");
+			}
+		} System.out.println();
+		System.out.print("You made it... and you have " + count + " children");
+	} 
 	}
-}
