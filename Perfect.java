d68778fadf6adaa52848eb33150c9b41303df2d2
Nadavycohen <128643278+Nadavycohen@users.noreply.github.com>
2024-01-04 06:40:43 -0500

    Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -4,5 +4,22 @@
 public class Perfect {
 	public static void main (String[] args) {
 		//// Put your code here
-	}
-}
+		int n = Integer.parseInt(args[0]);
+		int sum = 1;
+		String perfect = n + " is perfect number since " + n + " = 1";
+
+		for (int i =2; i < n; i++ ){
+			if (n%i==0){
+			sum += i ;
+			perfect = perfect + " + " + i;
+			}
+		}
+			if (sum==n){
+				System.out.print(perfect);
+			}
+			
+				else {
+					System.out.print(n + " is not a perfect number");
+				}
+			}
+		}
